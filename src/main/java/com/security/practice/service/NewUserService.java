65f3602ac30e9e15a2b4e40d77481880d7700858
kshitiz58 <kshitizbabik.37@gmail.com>
2024-05-25 18:37:05 +0545

    Signup and Login Page Added.

--- src/main/java/com/security/practice/service/NewUserService.java
+++ src/main/java/com/security/practice/service/NewUserService.java
@@ -1,6 +1,5 @@
 package com.security.practice.service;
 
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
